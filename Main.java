bdb266bde436277549c3ab0627202b2ea2583eb6
Ian Henderson <ian.henderson1215@gmail.com>
2023-01-29 01:06:16 -0800

    Finished Java Temperature Converter

--- Main.java
+++ Main.java
@@ -1,8 +1,11 @@
-import javax.sound.midi.Soundbank;
+
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
+        final DecimalFormat df = new DecimalFormat("0.00");
 
         //Start of application, letting them know this is a temperature converter, and how to
         //properly input the temp. Application will run by asking the type of temp they want to convert FROM
@@ -33,14 +36,14 @@ public class Main {
         }
 
         // confirming what we are converting FROM
-        System.out.printf("\nPerfect! We will be converting FROM " + tempConvertingFROM + "\n");
+        System.out.printf("\n**** Perfect! We will be converting FROM " + tempConvertingFROM + " ****\n");
 
         //Asking to put the number they want to convert
         System.out.printf("Please type in the number of the temperature you would you to convert: ");
         double temperatureNumber = Double.parseDouble(scanner.nextLine());
 
         //confirming the temp number
-        System.out.printf("\nAlright, we will be converting " + temperatureNumber + " degrees " + tempConvertingFROM);
+        System.out.printf("\n**** Alright, we will be converting " + temperatureNumber + " degrees " + tempConvertingFROM + " ****");
 
         //Getting what temp they want to convert TO
         System.out.println("\n\nFinally, please type in what temperature you would like to convert TO");
@@ -64,13 +67,36 @@ public class Main {
             System.exit(0);
         }
 
-        double finalTotal = 0;
+
+        //Conversion logic and equations
+    double finalTotal = 0.0;
 
         if(tempConvertingFROM.equalsIgnoreCase(tempConvertingTO)){
             System.out.println("\nYou choose the same type of temperature, please restart and choose two DIFFERENT temperature types");
-        } else
+            System.exit(0);
+        } else if (tempConvertingFROM.contains("F") && tempConvertingTO.contains("C")){
+            // F -> Csubtract 32 and multiply by .5556 (or 5/9).
+            finalTotal = (temperatureNumber - 32) * 0.5556;
+        } else if (tempConvertingFROM.contains("F") && tempConvertingTO.contains("K")){
+            // F -> K To convert Fahrenheit degrees into Kelvins, (°F − 32) × 5/9 + 273.15 = K.
+            finalTotal = ((temperatureNumber - 32) * 0.5556 + 273.15);
+        } else if (tempConvertingFROM.contains("C") && tempConvertingTO.contains("F")){
+            // C -> F     (°C × 9/5) + 32 = °F.
+            finalTotal = (temperatureNumber * 1.8) + 32;
+        } else if (tempConvertingFROM.contains("C") && tempConvertingTO.contains("K")){
+            // C -> K     °C + 273.15 = K
+            finalTotal = temperatureNumber + 273.15;
+        } else if (tempConvertingFROM.contains("K") && tempConvertingTO.contains("F")){
+            // K -> F   (K − 273.15) × 9/5 + 32 = °F.
+            finalTotal = (temperatureNumber - 273.15) * 1.8 + 32;
+        } else if (tempConvertingFROM.contains("K") && tempConvertingTO.contains("C")){
+            // K -> C      K − 273.15 = °C
+            finalTotal = temperatureNumber - 273.15;
+        }
 
 
-            System.out.println("");
+        System.out.println("**** " + temperatureNumber + " degrees " + tempConvertingFROM + " is equal to " + df.format(finalTotal) + " degrees " + tempConvertingTO + "! ****");
+        System.out.println("\n");
+        System.out.println("Thank you for using the temperature converter! \nPlease restart the application if you would like to use it again!");
     }
 }
